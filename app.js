0b4bd6074ea2eb4dea129249bd04a763d152f4da
andywijaya15 <andykevonata@gmail.com>
2022-08-08 16:28:49 +0700

    commit pertama socketio di github

---
+++ app.js
@@ -0,0 +1,27 @@
+import { createServer } from "http";
+import  {Server}  from "socket.io";
+
+const httpServer = createServer();
+
+const options = {
+  cors: {
+    origin: "https://ndik.helloworld.my.id",
+    // origin: "*",
+  },
+};
+
+const io = new Server(httpServer, options);
+
+io.use((socket, next) => {
+  next();
+});
+
+io.on("connection", (socket) => {
+  socket.on("connected", (data) => {
+    socket.emit("nyambung", `Welcome ${data.username}`);
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Hahaha nyala di 3000");
+});
